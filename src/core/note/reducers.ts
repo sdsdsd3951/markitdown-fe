8b2f5fc3842e7c56b3a52b66fb7b13fdbafb4ad8
Sina Montazeri <sinamonta@outlook.com>
2020-04-14 01:20:32 +0430

    update note reducers

--- src/core/note/reducers.ts
+++ src/core/note/reducers.ts
@@ -1,25 +1,24 @@
-import { handleActions } from "redux-actions"
-import { NoteState } from "./index"
-import { updateCurrentNote as loginStartAction } from "./actions"
-import { ContentState, convertToRaw } from "draft-js"
+import { handleActions } from "redux-actions";
+import { NoteState } from "./index";
+import { updateCurrentNote as loginStartAction } from "./actions";
 
-const updateCurrentNote = loginStartAction.toString()
+const updateCurrentNote = loginStartAction.toString();
 
-const placeHolder = convertToRaw(
-    ContentState.createFromText("# This is a header And this is a paragraph # This is a header nd this is a paragraph # This is a header And this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph")
-)
+const placeHolder =
+  "# This is a header And this is a paragraph # This is a header nd this is a paragraph # This is a header And this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph";
 
 const defaultState: NoteState = {
-    currentNote: JSON.stringify(placeHolder),
-    notes: [],
-}
+  currentNote: placeHolder,
+  notes: [],
+  status: 2,
+};
 
 const handlers = {
-    [updateCurrentNote]: (state: any, action: any) => ({
-        ...state,
-        currentNote: action.payload,
-    }),
-}
+  [updateCurrentNote]: (state: any, action: any) => ({
+    ...state,
+    currentNote: action.payload,
+  }),
+};
 
-const reducer = handleActions<NoteState, string>(handlers, defaultState)
-export default reducer
+const reducer = handleActions<NoteState, string>(handlers, defaultState);
+export default reducer;
