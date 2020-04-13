190a29340dbb12b37c6befb9f1cf44040d532927
Sina Montazeri <sinamonta@outlook.com>
2020-04-14 01:18:27 +0430

    clean up preview-note

--- src/components/preview-note/preview-note.tsx
+++ src/components/preview-note/preview-note.tsx
@@ -1,7 +1,4 @@
 import React, { FC } from "react"
-import { connect } from "react-redux"
-// import styled from "@emotion/styled"
-import { RootType } from "core/store"
 import ReactMarkdown from "react-markdown"
 import CodeBlock from "./render-code"
 
@@ -10,18 +7,10 @@ type Props = {
     value: string
 }
 
-const PreviewNoteComponent: FC<Props> = ({ value }) => {
+const PreviewNote: FC<Props> = ({ value }) => {
     return (
         <ReactMarkdown source={value} renderers={{ code: CodeBlock }} />
     )
 }
-
-const mapStateToProps = (state: RootType) => ({
-    value: state.note.currentNote,
-})
-
-const mapActionsToProps = () => ({})
-
-const PreviewNote = connect(mapStateToProps, mapActionsToProps)(PreviewNoteComponent)
 
 export default PreviewNote
