a32cc05c70e6f8d809b59a43baadaa6ebf7bd926
Sina Montazeri <sinamonta@outlook.com>
2020-04-15 14:10:12 +0430

    update test note

--- src/core/note/reducers.ts
+++ src/core/note/reducers.ts
@@ -4,8 +4,70 @@ import { updateCurrentNote as loginStartAction } from "./actions";
 
 const updateCurrentNote = loginStartAction.toString();
 
-const placeHolder =
-  "# This is a header And this is a paragraph # This is a header nd this is a paragraph # This is a header And this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph";
+const placeHolder =`
+  # H1
+  ## H2
+  ### H3
+  #### H4
+  ##### H5
+  ###### H6
+
+  Alt-H2 \n
+
+  horizental line:
+  ------  
+ ## Text Styling:
+  *This text will be italic* \n
+  _This will also be italic_ \n
+  **This text will be bold** \n
+  __This will also be bold__ \n
+  *You **can** combine them* \n
+
+
+## Here's a table:
+| Head | Head | Head | Head |
+| --- | --- | --- | --- |
+| Data | Data | Data | Data |
+| Data | Data | Data | Data |
+| Data | Data | Data | Data |
+
+## Un-ordered list:
+* Item 1
+* Item 2
+  * Item 2a
+  * Item 2b
+  * 
+## ordered list:
+1. Item 1
+2. Item 2
+3. Item 3
+  * Item 3a
+  * Item 3b
+  * 
+
+
+## BLOCKQUOTES
+  As Grace Hopper said:
+  > I’ve always been more interested
+  > in the future than in the past.
+
+## Images
+Example: ![Spotify Logo](https://logo.clearbit.com/spotify.com) \n
+
+## Links
+http://github.com - automatic! \n
+[GitHub](http://github.com)
+
+## Special Characters
+\\* literal asterisks \\* \n
+\\#literal hashtags\\# \n
+
+
+`
+
+
+
+
 
 const defaultState: NoteState = {
   currentNote: placeHolder,
