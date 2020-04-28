2d6f874d0b288ce07acb920c78cce59937c158c6
Sina Montazeri <sinamonta@outlook.com>
2020-04-28 18:55:56 +0430

    add create note view

--- src/components/side-bar/menu-actions.tsx
+++ src/components/side-bar/menu-actions.tsx
@@ -2,8 +2,8 @@ import React, { FC } from "react";
 import styled from "@emotion/styled";
 import { connect } from "react-redux";
 import { RootType } from "core/store";
-import { FaSearch } from 'react-icons/fa'
-import { MdSort } from 'react-icons/md'
+import { FaSearch, FaPlus } from "react-icons/fa";
+import { MdSort } from "react-icons/md";
 
 type Props = {
   theme?: any;
@@ -15,13 +15,12 @@ const Actions = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-  border-bottom: 1px solid #535353;
 `;
 
 const Action = styled.li`
   padding: 0;
 
-  .input-icons { 
+  .input-icons {
     margin-left: 9.5rem;
     margin-top: 0.95rem;
     position: absolute;
@@ -29,6 +28,7 @@ const Action = styled.li`
     color: #ccc;
     cursor: text;
   }
+  border-bottom: 1px solid #535353;
 `;
 
 const SearchBox = styled.input`
@@ -37,34 +37,61 @@ const SearchBox = styled.input`
   font-size: 1.4rem;
   width: 75%;
   padding: 0.5rem;
-  :hover{
-      background-color: #eee;
-    }
-`
+  :hover {
+    background-color: #eee;
+  }
+`;
 const Sort = styled.span`
-    font-size: 2rem;
-    margin: 0;
-    color: #888;
-    height:3.25rem;
-    :hover{
-      background-color: #eee;
-    }
-    display: inline-block;
-    width: 25%;
-    padding: 0 0.5rem 0 0.75rem;
-    i{
-      margin: 0 auto;
-    }
-`
-
+  font-size: 2rem;
+  margin: 0;
+  color: #888;
+  height: 3.25rem;
+  :hover {
+    background-color: #eee;
+  }
+  display: inline-block;
+  width: 25%;
+  padding: 0 0.5rem 0 0.75rem;
+  i {
+    margin: 0 auto;
+  }
+`;
+const NewNoteButton = styled.button`
+  background: #fff;
+  border: none;
+  box-shadow: none;
+  padding: 1rem;
+  font-size: 1.3rem;
+  width: 100%;
+  :hover {
+    background-color: #dfdfdf;
+  }
+  svg {
+    margin-right: 0.5rem;
+    font-size: 1.5rem;
+    color: #414141;
+  }
+  span {
+    color: #414141;
+  }
+`;
 
 const MenuActionsComponent: FC<Props> = (props) => {
   return (
     <Actions>
       <Action>
-        <FaSearch className="input-icons"/><SearchBox placeholder="Search notes" id="search-box"/>
-        <Sort><MdSort/></Sort>
-        </Action>
+        <NewNoteButton>
+          <FaPlus />
+          <span>New Note</span>
+        </NewNoteButton>
+      </Action>
+      <Action>
+        <FaSearch className="input-icons" />
+        <SearchBox placeholder="Search notes" id="search-box" />
+        <Sort>
+          <MdSort />
+        </Sort>
+      </Action>
     </Actions>
   );
 };
